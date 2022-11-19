c256c3828f6a7d018e80d5af90ed73e294fbbc9d
LODYGENSKY Oleg <oleg.lodygensky@lal.in2p3.fr>
2022-11-19 21:11:32 +0100

    TokenizedBallot as from Matheus

---
+++ contracts/TokenizedBallot.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Ballot {
+ 
+    struct Proposal {
+        bytes32 name;
+        uint256 voteCount;
+    }
+
+    Proposal[] public proposals;
+
+    constructor(bytes32[] memory proposalNames) {
+        for (uint256 i = 0; i < proposalNames.length; i++) {
+            proposals.push(Proposal({name: proposalNames[i], voteCount: 0}));
+        }
+    }
+
+    function vote(uint256 proposal, uint256 amount) external {
+        // implement the logic
+        proposals[proposal].voteCount += amount;
+    }
+
+    function winningProposal() public view returns (uint256 winningProposal_) {
+        uint256 winningVoteCount = 0;
+        for (uint256 p = 0; p < proposals.length; p++) {
+            if (proposals[p].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[p].voteCount;
+                winningProposal_ = p;
+            }
+        }
+    }
+
+    function winnerName() external view returns (bytes32 winnerName_) {
+        winnerName_ = proposals[winningProposal()].name;
+    }
+}
